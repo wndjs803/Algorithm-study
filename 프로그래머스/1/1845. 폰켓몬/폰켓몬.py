4fb69060938f3123686001e196a5df328521d705
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-12-10 13:31:18 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,8 @@
 def solution(nums):
-    type_count = len(set(nums))
-    length = len(nums)/2
+    type_count = len(set(nums)) # 폰켓몬 종류 수
+    length = len(nums)/2 # N/2 폰켓몬 선택하는 수
     
-    if(type_count <= length):
+    if(type_count <= length): # 폰켓몬의 종류가 선택해야 하는 수보다 작을 시
         answer = type_count
     else: answer = length
     
