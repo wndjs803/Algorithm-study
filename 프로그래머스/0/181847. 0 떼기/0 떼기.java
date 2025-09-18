dfd08bb21917ca73feda0b590cb0847e24e7f9c5
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2025-09-18 18:37:43 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String n_str) {
+        if (n_str.charAt(0) != '0') return n_str;
+        
+        for (int i = 0; i < n_str.length(); i++) {
+            if (n_str.charAt(i) != '0') {
+                return n_str.substring(i, n_str.length());
+            }
+        }
+        
+        return null;
+    }
+}
